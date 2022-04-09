deecb431ee0788a04c4ba058b3df7d4eb6e60587
Marlon-Dener <marlon.dener@voltbras.com.br>
2022-04-09 20:56:05 -0300

    Add auth with Github

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,26 +1,39 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, useSession, signOut } from 'next-auth/react'
+
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const { data: session, status } = useSession()
     
-    return isUserLoggedIn ? (
-        <button
-          type="button"
-          className={styles.signInButton}
-         >
-            <FaGithub color="#04d361"/>
-             Marlon Dener
-            <FiX color="#737380" className={styles.closeIcon} />
-        </button>
+    return session ? (
+        <div className={styles.signInContainer}>
+            <button
+              type="button"
+              className={styles.signInButton}
+            >
+                <FaGithub color="#04d361"/>
+                {status === "loading" ? "..." : session.user.name}
+            </button>
+            <div 
+              className={styles.closeSessionContainer}
+              onClick={() => signOut()}
+            >
+                <FiX 
+                  color="#cccccc"
+                  className={styles.closeIcon}
+                />
+            </div>
+        </div>
     ) : (
         <button
           type="button"
           className={styles.signInButton}
+          onClick={() => signIn('github')}
          >
             <FaGithub color="#eba417"/>
-            Sign in with Github
+            {status === "loading" ? "..." : "Sign in with Github" }
         </button>
     )
 }
