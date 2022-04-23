96ba32a8c37c3f25f84cf376c4a2cc7d48801b7a
Marlon-Dener <marlon.dener@voltbras.com.br>
2022-04-22 23:36:32 -0300

    List posts in screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
 import { SignInButton } from './SignInButton'
 import styles from './styles.module.scss'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header () {
+    const { asPath } = useRouter()
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
+                  <ActiveLink
+                    href='/' 
+                    activeClassName={styles.active}
+                   >
                     <a className={styles.active}>Home</a>
+                  </ActiveLink>
+                  <ActiveLink 
+                    href='/posts' 
+                    prefetch 
+                    activeClassName={styles.active}
+                  >
                     <a>Posts</a>
+                  </ActiveLink>
                 </nav>
                <SignInButton />
             </div>
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+interface PostsProps {
+  posts: Post[]
+}
+ 
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -10,32 +24,45 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href=''>
-                        <time>12 de maio de 2021</time>
-                        <strong>There are many variations of passages of Lorem Ipsum available.</strong>
-                        <p>It is a long established fact that a reader will be distracted by the readable
-                           content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-                           more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
-                           making it look like readable English. </p>
-                    </a>
-                    <a href=''>
-                        <time>12 de maio de 2021</time>
-                        <strong>There are many variations of passages of Lorem Ipsum available.</strong>
-                        <p>It is a long established fact that a reader will be distracted by the readable
-                           content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-                           more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
-                           making it look like readable English. </p>
-                    </a>
-                    <a href=''>
-                        <time>12 de maio de 2021</time>
-                        <strong>There are many variations of passages of Lorem Ipsum available.</strong>
-                        <p>It is a long established fact that a reader will be distracted by the readable
-                           content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
-                           more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
-                           making it look like readable English. </p>
-                    </a>
+                    { posts.map(post => (
+                     <a href='#' key={post.slug}>
+                        <time>{post.updatedAt}</time>
+                        <strong>{post.title}</strong>
+                        <p>{post.excerpt}</p>
+                    </a>                       
+                    ))}
                 </div>
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+       
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'publications')
+      ], {
+          fetch: ['publications.title', 'publications.content'],
+      }
+    )
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+    
+    return {
+        props: {
+            posts,
+        }
+    }
 }
